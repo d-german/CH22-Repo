dee0f2611f1bbd5b5b6109d0dca8510a07e4bcb5
d-german <facmet107@gmail.com>
2018-11-10 14:46:34 -0600

    made some changes

---
+++ CH22RepoPattern/Models/IBooksRepository.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CH22RepoPattern.Models
+{
+    public interface IBooksRepository
+    {
+        IEnumerable<AuthorAndTitle> GetAuthorsAndTitles();
+        IEnumerable<AuthorAndISBN> GetAuthorsAndISBNs();
+        bool Add(RepoAuthor repoAuthor, params RepoTitle[] repoTitles);
+    }
+
+    class BooksRepository : IBooksRepository
+    {
+        private readonly BooksDbEntities _dbContext = new BooksDbEntities();
+
+
+        public IEnumerable<AuthorAndTitle> GetAuthorsAndTitles()
+        {
+            return
+                from book in _dbContext.Titles
+                from author in book.Authors
+                orderby author.LastName, author.FirstName, book.Title1
+                select new AuthorAndTitle
+                {
+                    FirstName = author.FirstName,
+                    LastName = author.LastName,
+                    Title = book.Title1
+                };
+        }
+
+        public IEnumerable<AuthorAndISBN> GetAuthorsAndISBNs()
+        {
+            return
+                from author in _dbContext.Authors
+                from title in author.Titles
+                orderby author.LastName, author.FirstName
+                select new AuthorAndISBN
+                {
+                    FirstName = author.FirstName,
+                    LastName = author.LastName,
+                    ISBN = title.ISBN
+                };
+        }
+
+        public bool Add(RepoAuthor author, params RepoTitle[] titles)
+        {
+            try
+            {
+                var authors = _dbContext.Authors;
+                var authorDb = new Author
+                {
+                    FirstName = author.FirstName,
+                    LastName = author.LastName,
+                    Titles = new List<Title>()
+                };
+
+                foreach (var title in titles)
+                {
+                    authorDb.Titles.Add(
+                        new Title
+                        {
+                            Title1 = title.BookTitle,
+                            Copyright = title.Copyright,
+                            ISBN = title.ISBN,
+                            EditionNumber = title.EditionNumber
+                        });
+                }
+
+                authors.Add(authorDb);
+                _dbContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+
+            return true;
+        }
+    }
+
+
+    public class RepoTitle
+    {
+        public RepoTitle()
+        {
+            Authors = new List<RepoAuthor>();
+        }
+
+        public string ISBN { get; set; }
+        public string BookTitle { get; set; }
+        public int EditionNumber { get; set; }
+        public string Copyright { get; set; }
+
+        public List<RepoAuthor> Authors { get; set; }
+    }
+
+    public class RepoAuthor
+    {
+        public RepoAuthor()
+        {
+            Titles = new List<RepoTitle>();
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public List<RepoTitle> Titles { get; set; }
+    }
+
+    public class AuthorAndISBN
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ISBN { get; set; }
+    }
+
+    public class AuthorAndTitle
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ CH22RepoPattern/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CH22RepoPattern.Models;
+
+namespace CH22RepoPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var booksRepo = new BooksRepository();
+
+            booksRepo.Add(
+                new RepoAuthor
+                {
+                    FirstName = "Damon",
+                    LastName = "German"
+                },
+                new RepoTitle
+                {
+                    BookTitle = "Why it's Hard to Write Clean Code",
+                    Copyright = "1998",
+                    EditionNumber = 5,
+                    ISBN = GetRandomString(rnd)
+                },
+                new RepoTitle
+                {
+                    BookTitle = "Why Map is my Favorite Data Structure",
+                    Copyright = "2007",
+                    EditionNumber = 1,
+                    ISBN = GetRandomString(rnd)
+                }
+            );
+
+            DisplayAuthorsAndTitles(booksRepo);
+            DisplayAuthorsAndISBNs(booksRepo);
+        }
+
+        private static void DisplayAuthorsAndTitles(IBooksRepository booksRepo)
+        {
+            var buf = new StringBuilder();
+
+            // display authors and titles in tabular format
+            foreach (var element in booksRepo.GetAuthorsAndTitles())
+                buf.Append($"\r\n\t{element.FirstName,-10} " +
+                           $"{element.LastName,-10} {element.Title}");
+
+            Console.WriteLine(buf.ToString());
+        }
+
+        private static void DisplayAuthorsAndISBNs(IBooksRepository booksRepo)
+        {
+            var buf = new StringBuilder();
+
+            // display authors and ISBNs in tabular format
+            foreach (var element in booksRepo.GetAuthorsAndISBNs())
+                buf.Append($"\r\n\t{element.FirstName,-10} " +
+                           $"{element.LastName,-10} {element.ISBN,-10}");
+
+            Console.WriteLine(buf.ToString());
+        }
+
+        public static string GetRandomString(Random rnd, int length = 15)
+        {
+            
+            var charPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw xyz1234567890";
+            var rs = new StringBuilder();
+
+            while (length > 0)
+            {
+                rs.Append(charPool[(int)(rnd.NextDouble() * charPool.Length)]);
+                length--;
+            }
+
+            return rs.ToString();
+        }
+    }
+}
